02f55f976038d277e39ea3b443eed309e9a07567
zhangtao <28449484@qq.com>
2019-04-12 00:24:20 +0800

    #

--- src/views/pages/detail/index.vue
+++ src/views/pages/detail/index.vue
@@ -2,7 +2,6 @@
     <div class="detail">
         <div class="base">
             <vue-photo-zoom-pro class="img" :scale='1.5'    :url="img"></vue-photo-zoom-pro >
-            11
             <commodityOption :id="id"/>
         </div>
         <phoneParameter/>
